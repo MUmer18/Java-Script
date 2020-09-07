baff2eb6b0ded6dec9677666dc278e2dc6ff8c98
MUmer04 <umer_1309e@aptechgdn.com>
2020-09-07 20:45:21 +0500

    js variables done

--- demo1.js
+++ demo1.js
@@ -1,1 +1,10 @@
-document.write("<h1>Hello World</h1>")
+//document.write("<h1>Hello World</h1>")
+//this is a comment
+/* this is a multi line cpmment 
+............
+*/
+//variables
+var num = 5; //this is a number
+var name = "Java script"; // string
+var flag = true ;//boolean
+alert(num) ;
