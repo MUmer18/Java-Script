9ca5083fff58f415db7f1f8e943cd626204f6884
MUmer04 <umer_1309e@aptechgdn.com>
2020-09-09 20:08:59 +0500

    functions, arrays, done

--- demo4.js
+++ demo4.js
@@ -18,9 +18,11 @@
 // addNumber("Muhammad ","Umer")
 
 function addNumber(a, b){
-   var total = a + b;
+   var total = a + b; //local variab;le
    return total;
 }
-
+var number = 7; //global variable
 var name = addNumber("Muhammad ","Umer");
-document.write("<h1>"+name+"</h1>")
+document.write("<h1>"+name+"</h1>");
+//document.write(total); //it will not display anything because it is locally declared
+document.write(number); 
---
+++ demo5.js
@@ -0,0 +1,5 @@
+var arr = ["car","bike","cycle"];
+arr.push("boat");
+for(var i = 0; i < arr.length; i++){
+    document.write("<h1>"+ arr[i]+ "</h1>")
+}
