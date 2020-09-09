fdd16c4647739e9b726553e704495ab100282dda
MUmer04 <umer_1309e@aptechgdn.com>
2020-09-09 19:16:48 +0500

    switch case done

--- demo1.js
+++ demo1.js
@@ -4,7 +4,18 @@
 ............
 */
 //variables
-var num = 5; //this is a number
-var name = "Java script"; // string
-var flag = true ;//boolean
-alert(num) ;
+// var num = 5; //this is a number
+// var name = "Java script"; // string
+// var flag = true ;//boolean
+// alert(num) ;
+ 
+///operators
+var nam1 = "Muhammad";
+var nam2 = "Umer";
+var fullName = nam1 + " " + nam2;
+alert(fullName);
+var a = 10;
+var b = 20;
+var result = a + b;
+document.write(result)
+alert(result)
---
+++ demo2.js
@@ -0,0 +1,48 @@
+//document.write("<h1>if else control statement </h1>");
+// a = 5;
+// if(a%2 == 0){
+//     document.write("the number is even");
+// }
+// else{
+//     document.write("the number is odd");
+///}
+// var a = 10;
+// if(a > 0){
+//     document.write("<h1>the number is positive</h1>");
+//     if(a%2 == 0){
+//         document.write("<h1>the number is even</h1>");
+//     }
+//     else{
+//         document.write("the number is oodd");
+//     }
+// }
+// else{
+//     document.write("the number is negative");
+// }
+//switch statement
+var day = 7
+switch(day){
+    case 1:
+    document.write("Monday");
+    break;
+    case 2:
+    document.write("Tuesday");
+    break;
+    case 3:
+    document.write("Wednesday");
+    break;
+    case 4:
+    document.write("Thursday");
+    break;
+    case 5:
+    document.write("Friday");
+    break;
+    case 6:
+    document.write("Saurday");
+    break;
+    case 7:
+    document.write("Sunday");
+    break;
+    default:
+    document.write("Wrong Inut")
+}
